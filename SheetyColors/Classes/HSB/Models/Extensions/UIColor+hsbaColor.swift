923a614084cd6e2a77eb0ea3e84827efa2a175b2
Christoph Wendt <christoph.wendt@me.com>
2019-04-23 21:00:07 +0200

    tests: verify results UIColor extension calls

--- SheetyColors/Classes/HSB/Models/Extensions/UIColor+hsbaColor.swift
+++ SheetyColors/Classes/HSB/Models/Extensions/UIColor+hsbaColor.swift
@@ -12,7 +12,7 @@ public extension UIColor {
     /// The HSBAColor representation of the UIColor instance.
     var hsbaColor: HSBAColor {
         func normalize(_ component: CGFloat, multiplier: CGFloat) -> CGFloat {
-            let nomralizedValue = component * multiplier
+            let nomralizedValue = floor(component * multiplier)
 
             if nomralizedValue > multiplier { return multiplier }
             if nomralizedValue < 0.0 { return 0.0 }
