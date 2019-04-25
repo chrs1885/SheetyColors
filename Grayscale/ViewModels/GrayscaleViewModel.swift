16f08ff4d93a5a78d67fd0eff9e106f89ade9c0b
Christoph Wendt <christoph.wendt@me.com>
2019-04-25 20:26:42 +0200

    chore: support grayscale type in factory

--- Grayscale/ViewModels/GrayscaleViewModel.swift
+++ Grayscale/ViewModels/GrayscaleViewModel.swift
@@ -13,7 +13,7 @@ class GrayscaleViewModel {
     var isAlphaEnabled: Bool
     var colorModel: GrayscaleColor
     weak var viewModelDelegate: SheetyColorsViewModelDelegate?
-    
+
     init(withColorModel colorModel: GrayscaleColor, alphaEnabled: Bool) {
         self.colorModel = colorModel
         isAlphaEnabled = alphaEnabled
@@ -24,40 +24,40 @@ extension GrayscaleViewModel: SheetyColorsViewModelProtocol {
     var primaryKeyText: String {
         return "Grayscale"
     }
-    
+
     var primaryValueText: String {
         return "\(Int(colorModel.white)) \(Int(colorModel.alpha))%"
     }
-    
+
     var secondaryKeyText: String {
         return "HEX"
     }
-    
+
     var secondaryValueText: String {
         return colorModel.hexColor
     }
-    
+
     var previewColorModel: SheetyColorProtocol {
         return colorModel
     }
-    
+
     var numberOfSliders: Int {
         let maxSliderCount = SliderType.allCases.count
-        
+
         return isAlphaEnabled ? maxSliderCount : maxSliderCount - 1
     }
-    
+
     func rainbowEnabled(forSliderAt _: Int) -> Bool {
         return false
     }
-    
+
     func stepInterval(forSliderAt _: Int) -> CGFloat {
         return 1.0
     }
-    
+
     func value(forSliderAt index: Int) -> CGFloat {
         guard let slider = SliderType(rawValue: index) else { fatalError() }
-        
+
         switch slider {
         case .white:
             return colorModel.white
@@ -65,10 +65,10 @@ extension GrayscaleViewModel: SheetyColorsViewModelProtocol {
             return colorModel.alpha
         }
     }
-    
+
     func maximumValue(forSliderAt index: Int) -> CGFloat {
         guard let slider = SliderType(rawValue: index) else { fatalError() }
-        
+
         switch slider {
         case .white:
             return 255.0
@@ -76,10 +76,10 @@ extension GrayscaleViewModel: SheetyColorsViewModelProtocol {
             return 100.0
         }
     }
-    
+
     func minimumColorModel(forSliderAt index: Int) -> SheetyColorProtocol {
         guard let slider = SliderType(rawValue: index) else { fatalError() }
-        
+
         switch slider {
         case .white:
             return GrayscaleColor(white: 0.0, alpha: 100.0)
@@ -87,10 +87,10 @@ extension GrayscaleViewModel: SheetyColorsViewModelProtocol {
             return GrayscaleColor(white: 255.0, alpha: 100.0)
         }
     }
-    
+
     func maximumColorModel(forSliderAt index: Int) -> SheetyColorProtocol {
         guard let slider = SliderType(rawValue: index) else { fatalError() }
-        
+
         switch slider {
         case .white:
             return GrayscaleColor(white: 255.0, alpha: 100.0)
@@ -98,10 +98,10 @@ extension GrayscaleViewModel: SheetyColorsViewModelProtocol {
             return GrayscaleColor(white: colorModel.white, alpha: 100.0)
         }
     }
-    
+
     func thumbText(forSliderAt index: Int) -> String? {
         guard let slider = SliderType(rawValue: index) else { fatalError() }
-        
+
         switch slider {
         case .white:
             return "W"
@@ -109,22 +109,21 @@ extension GrayscaleViewModel: SheetyColorsViewModelProtocol {
             return "%"
         }
     }
-    
+
     func thumbIconName(forSliderAt _: Int) -> String? {
         return nil
     }
-    
+
     func sliderValueChanged(forSliderAt index: Int, value: CGFloat) {
         guard let slider = SliderType(rawValue: index) else { fatalError() }
-        
+
         switch slider {
         case .white:
             colorModel.white = floor(value)
         case .alpha:
             colorModel.alpha = floor(value)
         }
-        
+
         viewModelDelegate?.didUpdateColorComponent(in: self)
     }
 }
-
