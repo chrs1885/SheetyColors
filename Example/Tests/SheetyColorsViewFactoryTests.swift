0ef6c3b9fc5ddc090acd83a097179fc5fd7c6d48
Christoph Wendt <christoph.wendt@me.com>
2019-04-26 22:50:18 +0200

    tests: verify that correct picker gets created by factory based on color type

--- Example/Tests/SheetyColorsViewFactoryTests.swift
+++ Example/Tests/SheetyColorsViewFactoryTests.swift
@@ -30,6 +30,42 @@ class SheetyColorsViewFactoryTests: QuickSpec {
                     expect(viewModel.colorModel).to(equal(testConfig.initialColor.rgbaColor))
                 }
             }
+
+            context("when calling create with a config of type .hsb") {
+                var testConfig: SheetyColorsConfigProtocol!
+                var view: SheetyColorsView!
+
+                beforeEach {
+                    testConfig = SheetyColorsConfigMock(alphaEnabled: true, initialColor: .red, hapticFeedbackEnabled: true, title: "", type: .hsb)
+                    view = SheetyColorsViewFactory.createView(withConfig: testConfig)
+                }
+
+                it("returns a SheetyColorsView with HSB components") {
+                    expect(view.viewModel).to(beAnInstanceOf(HSBViewModel.self))
+
+                    let viewModel = view.viewModel as! HSBViewModel
+                    expect(viewModel.isAlphaEnabled).to(equal(testConfig.alphaEnabled))
+                    expect(viewModel.colorModel).to(equal(testConfig.initialColor.hsbaColor))
+                }
+            }
+
+            context("when calling create with a config of type .grayscale") {
+                var testConfig: SheetyColorsConfigProtocol!
+                var view: SheetyColorsView!
+
+                beforeEach {
+                    testConfig = SheetyColorsConfigMock(alphaEnabled: true, initialColor: .red, hapticFeedbackEnabled: true, title: "", type: .grayscale)
+                    view = SheetyColorsViewFactory.createView(withConfig: testConfig)
+                }
+
+                it("returns a SheetyColorsView with HSB components") {
+                    expect(view.viewModel).to(beAnInstanceOf(GrayscaleViewModel.self))
+
+                    let viewModel = view.viewModel as! GrayscaleViewModel
+                    expect(viewModel.isAlphaEnabled).to(equal(testConfig.alphaEnabled))
+                    expect(viewModel.colorModel).to(equal(testConfig.initialColor.grayscaleColor))
+                }
+            }
         }
     }
 }
