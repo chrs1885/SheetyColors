67d66555d5f9bc40d1e9a6a3505715f81c400ec5
Christoph Wendt <christoph.wendt@me.com>
2019-08-06 16:20:11 +0200

    chore: adjust Package.swift to produce lib (draft)

--- Package.swift
+++ Package.swift
@@ -8,9 +8,20 @@ let package = Package(
         .package(url: "https://github.com/Realm/SwiftLint", from: "0.34.0"),
         .package(url: "https://github.com/orta/Komondor", from: "1.0.4"),
         .package(url: "https://github.com/eneko/SourceDocs.git", from: "0.5.1"),
+        .package(url: "https://github.com/chrs1885/Capable.git", from: "1.0.1"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2")
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.5.0"),
+    ],
+    products: [
+        .library(
+            name: "SheetyColors",
+            targets: ["SheetyColors"])
     ],
     targets: [
         .target(name: "SheetyColors_Example", dependencies: [], path: "Example", sources: ["SheetyColors/SheetType.swift"]),
+        .target(name: "SheetyColors", dependencies: ["Capable"], path: "SheetyColors"),
+        .testTarget(name: "SheetyColors_Tests", dependencies: ["SheetyColors", "Quick", "Nimble", "SnapshotTesting"], path: "Example/Tests"),
     ]
 )
 
