c06dde8c9e93207a38e945d75ad5dde680cb3546
Christoph Wendt <christoph.wendt@dm.de>
2020-02-25 22:27:20 +0100

    add swiftLanguageVersions

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/Realm/SwiftLint", from: "0.34.0"),
         .package(url: "https://github.com/orta/Komondor", from: "1.0.4"),
         .package(url: "https://github.com/eneko/SourceDocs.git", from: "0.5.1"),
-        .package(url: "https://github.com/chrs1885/Capable.git", from: "1.0.1"),
+        .package(url: "https://github.com/chrs1885/Capable.git", from: "1.1.3"),
         .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2")
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.5.0"),
@@ -22,6 +22,9 @@ let package = Package(
         .target(name: "SheetyColors_Example", dependencies: [], path: "Example", sources: ["SheetyColors/SheetType.swift"]),
         .target(name: "SheetyColors", dependencies: ["Capable"], path: "SheetyColors"),
         .testTarget(name: "SheetyColors_Tests", dependencies: ["SheetyColors", "Quick", "Nimble", "SnapshotTesting"], path: "Example/Tests"),
+    ],
+    swiftLanguageVersions: [
+        .v5,
     ]
 )
 
