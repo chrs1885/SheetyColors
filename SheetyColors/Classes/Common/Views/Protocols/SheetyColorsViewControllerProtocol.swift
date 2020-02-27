5a4d67db8aac26097ec271d52c80c137a19bc9b5
Christoph Wendt <christoph.wendt@dm.de>
2020-02-27 23:29:19 +0100

    chore: update dependencies

---
+++ SheetyColors/Classes/Common/Views/Protocols/SheetyColorsViewControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SheetyColorsViewControllerProtocol.swift
+//  SheetyColors
+//
+//  Created by Christoph Wendt on 03.02.19.
+//
+
+import UIKit
+
+protocol SheetyColorsViewControllerProtocol {
+    var previewColor: UIColor { get }
+}
